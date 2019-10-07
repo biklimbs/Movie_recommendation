ba70e153dbdf7f676ba7fa8d9bdb9937f5f212c9
Bikash Limboo <bikash.limboo@vmokshagroup.com>
2019-10-07 16:37:40 +0530

    Main file changed

--- SCRIPTS/recommendation_test.py
+++ SCRIPTS/recommendation_test.py
@@ -33,11 +33,20 @@ __maintainer__="Bikash Limboo"
 #---main function---
 def main():
     try:
-        #Reading users file:
+        #---Reading users file:
         u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
         users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,encoding='latin-1')
-        print(users)
-        pass
+        print(users.head())
+        #---Reading ratings file:
+        r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+        ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,encoding='latin-1')
+        
+        #Reading items file:
+        i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+        'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+        'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+        items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,encoding='latin-1')
+        print(items.head())
     except Exception as e:
         log.error(colored(str(e),"red"))
 
